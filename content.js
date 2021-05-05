fdcc8493f8af26b01b7f1bc2c0cf9f9cf0fd736b
vodchella <twister.kz@gmail.com>
2021-05-05 17:44:53 +0600

    Get rid of OL's first line blinking

--- content.js
+++ content.js
@@ -1,6 +1,12 @@
-$(document).ready(function(){
-    var li = $(document).find('html:eq(0)>body:eq(0)>div#invisible>ol:eq(0)>li:eq(0)')
-    if (li.length) {
-        li.html('Тут будет имя победителя')
+const newValue = 'Тут будет имя победителя';
+
+var html = $(document).find('html:eq(0)')
+html.bind('DOMSubtreeModified', function(e) {
+    if (e.target.innerHTML.length > 0) {
+        var li = html.find('body:eq(0)>div#invisible>ol:eq(0)>li:eq(0)')
+        if (li.length && li.html() !== newValue) {
+            html.bind('DOMSubtreeModified', function(e1) {});
+            li.html(newValue)
+        }
     }
-})
+});
